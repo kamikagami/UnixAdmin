060d6ea33d1bc52caa945b3dd8b7c62288ba2b3e
Kay <Kay@example.com>
2014-09-04 00:55:11 +1000

    option3

--- backup.sh
+++ backup.sh
@@ -8,6 +8,8 @@ INPUT=
 DIRECTORY=
 FILESNAME=
 DATE=
+ARCMTD=
+
 
 #Script
 
@@ -22,11 +24,24 @@ DATE=
         $ECHO "8.   Encrypt backup archive"
         $ECHO "9.   Decrypt backup archive"
         $ECHO "10.  EXIT"
-
+        #TODO limit number 1-10
         read -p "Enter Choice: " INPUT
 
-        echo $INPUT
 if [ "$INPUT" == "1" ]; then
-    echo "hello"
+   #TODO: need validation here
     read -p "Please type the directory to be backed up: " $DIRECTORY
+elif [ "$INPUT" == "2" ]; then
+    $ECHO "Please select which files to backup"
+    $ECHO "a. Backup All."
+    $ECHO "b. Backup by selected Date"
+    read -p "Enter Choice: " INPUT
+    if [ "$INPUT" == "a" ];then
+        $FILESNAME=*.*
+    elif [ "$INPUT" == "b" ];then
+        read -p "Please type a date, we will only backup files that were last modified before it." DATE
+    fi
+elif [ "$INPUT" == "3" ];then
+    read -p "Please select your archive method" ARCMTD
 fi
+
+
